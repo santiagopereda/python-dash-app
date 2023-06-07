159fc970ad90dc69f9e2753fd5f955f66d2b5ba7
Santiago Pereda <santiago.pereda1@gmail.com>
2023-06-07 21:15:08 +0200

    Update: optimized functions to prepare data

--- src/data/explore_data.py
+++ src/data/explore_data.py
@@ -1,35 +1,54 @@
 from functions.data_cleaning import *
+import pickle
 
+# ======================================================================================================================
+# Specify CSV Location and create a Pandas Dataframe 
+# ======================================================================================================================
 df_location = '../../data/raw/earth_challenge_dataset.csv'
+df = pd.read_csv(df_location, parse_dates=['DateStandardized'])
 
-df = pd.read_csv(df_location)
-
+# ======================================================================================================================
+# Splits a specified column, adds a hyphen, and joins it back together.
+# ======================================================================================================================
 df = split_column_by_hyphen(df, 'ISO_CODE')
 
-null_df = get_null_percentage(df)
-print("Data Null Percentage by Column")
-print(null_df.sort_values(by='Null Percentage', ascending=False))
-print(" ")
-
+# ======================================================================================================================
+# Update missing values in the 'COUNTRY' columns based on a country list.
+# ======================================================================================================================
 df = update_countries(df)
 
+# ======================================================================================================================
+# Fills missing values in the 'Location' column by retrieving location information
+# using latitude and longitude values for each country.
+# ======================================================================================================================
 df = fill_null_countries(df)
 
-df = fill_country_code(df)
-
+# ======================================================================================================================
+# Fills missing values in the 'ISO_SUB', 'NAME', and 'LAND_TYPE' columns based on the 'Location' column using pycountry.
+# ======================================================================================================================
 df = fill_country_subdivision(df)
 
+# ======================================================================================================================
+# Drop Columns with repeated data or more than 90% missing values
+# ======================================================================================================================
 df = df.drop(index=df[(df['COUNTRY'].isna())].index)
 df = df.drop(index=df[df['Location'].isna()].index)
 df = df.drop(index=df[df['ISO_SUB'].isna()].index)
-
 drop_list = ['TotalArea_Sq_m', 'Other', 'FieldObsevations', 'BeachAreaLandcover', 'BeachType', 'DebrisDescription',
              'WaterfrontName', 'TotalWidth_m', 'StartTime', 'Longitude2', 'ShorelineName', 'Latitude2', 'X', 'Y',
-             'UniqueID', 'SourceID', 'SubCountry_L1_FromSource', 'SubCountry_L2_FromSource', 'CountryName_FromSource'
-             'OBJECTID']
+             'UniqueID', 'SourceID', 'SubCountry_L1_FromSource', 'SubCountry_L2_FromSource', 'CountryName_FromSource',
+             'OBJECTID', "ISO_SUB", "ISO_CC", "ADMINTYPE", "RecordSequenceID", "DateOriginal", "MonthYear", "Year",
+             "MonthNum", "Month", "Day", "DOW", "COUNTRYAFF", "DISPUTED", "NOTES", "AUTONOMOUS"]
+df = df.drop(drop_list, axis=1)
 
-null_df = get_null_percentage(df)
-print(" ")
-print("Data Null Percentage by Column")
-print(null_df.sort_values(by='Null Percentage', ascending=False))
-print(" ")
+# ======================================================================================================================
+# Set Date as index
+# ======================================================================================================================
+df["DateStandardized"] = df["DateStandardized"].dt.date
+df = df.set_index(df["DateStandardized"]).sort_index()
+df.index.name = None
+
+# ======================================================================================================================
+# Export dataset
+# ======================================================================================================================
+df.to_pickle("../../data/interim/01_data_processed.pkl")
--- src/data/functions/data_cleaning.py
+++ src/data/functions/data_cleaning.py
@@ -1,56 +1,77 @@
 import re
 import sys
-import shutil
-import pandas as pd
-import pycountry
 import geopy
+import shutil
+import pycountry
+import pandas as pd
+from unidecode import unidecode
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 pd.options.display.max_rows = 100
 
 
-def get_null_percentage(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Calculate the null percentage for each column in the DataFrame.
-    Parameters:
-        df (pd.DataFrame): The input DataFrame.
-    Returns:
-        pd.DataFrame: A DataFrame with two columns: 'Column Name' and 'Null Percentage'.
-    """
-    null_percentage = (df.isnull().sum() / len(df)) * 100
-    null_df = pd.DataFrame(
-        {'Column Name': null_percentage.index, 'Null Percentage': null_percentage.values})
-    null_df.set_index('Column Name', inplace=True)
-    null_df.index.name = None
-    return null_df
-
-
 def split_column_by_hyphen(df: pd.DataFrame, column: str) -> pd.DataFrame:
     """
-    Splits a specified column, adds a hyphen if it doesn't exist, and joins it back together.
-
+    Splits a specified column, adds a hyphen, and joins it back together.
     Parameters:
         df (pd.DataFrame): The input DataFrame.
         column (str): The name of the column to split and join.
-
     Returns:
         pd.DataFrame: The DataFrame with the updated column.
     """
-    # Convert the specified column to string type
-    df[column] = df[column].astype(str).apply(
-        lambda x: x if '-' in x else x[:2] + '-' + x[2:] if len(x) >= 4 else x)
+    # Convert the specified column to string type, split each value, add a hyphen, and join them back together
+    df[column] = df[column].astype(str).apply(lambda x: x[:2] + '-' + x[2:])
+    return df
 
+
+def update_countries(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Update missing values in the 'COUNTRY' and 'COUNTRYAFF' columns based on a country list.
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
+        column_list (list): A list of column names to iterate over.
+    Returns:
+        pd.DataFrame: The DataFrame with updated 'COUNTRY' columns.
+    """
+    column_list = ['Location', 'CountryName_FromSource']
+
+    country_list = set()
+
+    # Collect unique country values from specified columns
+    for column in column_list:
+        country_list |= set(df[df['COUNTRY'].isnull()][column].unique())
+
+    # Iterate over country values
+    for country in country_list:
+        # Iterate over specified columns
+        for column in column_list:
+            # Get subset of rows where 'COUNTRY' is null and 'column' matches the country value
+            subset = df[(df['COUNTRY'].isnull()) & (df[column] == country)]
+
+            # Iterate over subset rows
+            for index, row in subset.iterrows():
+                # Check for matching country name, alpha-3 code, or alpha-2 code
+                for country_name in pycountry.countries:
+                    country_lower = country.strip().lower()
+
+                    if country_name.name.lower() in country_lower:
+                        df.at[index, 'COUNTRY'] = country_name.name
+                        break
+                    if country_name.alpha_3 in country_lower:
+                        df.at[index, 'COUNTRY'] = country_name.name
+                        break
+                    if country_name.alpha_2 in country_lower:
+                        df.at[index, 'COUNTRY'] = country_name.name
+                        break
     return df
 
 
 def update_location(df: pd.DataFrame, index: int) -> None:
     """
     Updates the location information for a single row based on latitude and longitude values.
-
     Parameters:
         df (pd.DataFrame): The DataFrame containing the row to update.
         index (int): The index of the row to update.
-
     Returns:
         None
     """
@@ -76,11 +97,6 @@ def update_location(df: pd.DataFrame, index: int) -> None:
 
             if 'country' in raw_data:
                 df.at[index, 'COUNTRY'] = raw_data['country']
-                df.at[index, 'COUNTRYAFF'] = raw_data['country']
-
-            if 'country_code' in raw_data:
-                df.at[index, 'ISO_CC'] = raw_data['country_code'].upper()
-                df.at[index, 'ISO3166-2-lvl4'] = raw_data['ISO3166-2-lvl4']
     except (GeocoderTimedOut, KeyError, ValueError):
         # Handle specific geocoding errors
         pass
@@ -90,14 +106,16 @@ def fill_null_countries(df: pd.DataFrame) -> pd.DataFrame:
     """
     Fills missing values in the 'Location' column by retrieving location information
     using latitude and longitude values for each country.
-
     Parameters:
         df (pd.DataFrame): The input DataFrame.
-
     Returns:
         pd.DataFrame: The DataFrame with the updated 'Location', 'NAME', 'COUNTRY',
-        'COUNTRYAFF', 'ISO_CC', and 'ISO3166-2-lvl4' columns.
+        'COUNTRYAFF', and 'ISO_CC' columns.
     """
+
+    def echo(string, padding=80):
+        padding = " " * (padding - len(string)) if padding else ""
+        print(string + padding, end='\r')
 
     # Get a list of countries with null values in the 'Location' column
     country_list = df[df['Location'].isnull()]['COUNTRY'].unique()
@@ -114,7 +132,8 @@ def fill_null_countries(df: pd.DataFrame) -> pd.DataFrame:
             update_location(df, index)
 
             i += 1
-            print(f"{i}/{country_len} {row['COUNTRY']}", end="\r")
+            progress = (f"{i}/{country_len} {row['COUNTRY']}")
+            echo(progress)
 
     # Get a subset of the DataFrame for rows with null values in both 'Location' and 'COUNTRY' columns
     null_df = df[df['Location'].isnull() & df['COUNTRY'].isnull()]
@@ -126,7 +145,8 @@ def fill_null_countries(df: pd.DataFrame) -> pd.DataFrame:
         update_location(df, index)
 
         i += 1
-        print(f"{i}/{null_len} {row['COUNTRY']}", end="\r")
+        progress = (f"{i}/{null_len} {row['COUNTRY']}")
+        echo(progress)
 
     # Get a list of countries with null values in the 'Location' column
     country_list = df[df['COUNTRY'].isnull(
@@ -145,105 +165,27 @@ def fill_null_countries(df: pd.DataFrame) -> pd.DataFrame:
             update_location(df, index)
 
             i += 1
-            print(f"{i}/{country_len} {row['COUNTRY']}", end="\r")
-
-    return df
-
-
-def update_countries(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Update missing values in the 'COUNTRY' and 'COUNTRYAFF' columns based on a country list.
-
-    Parameters:
-        df (pd.DataFrame): The input DataFrame.
-        column_list (list): A list of column names to iterate over.
-
-    Returns:
-        pd.DataFrame: The DataFrame with updated 'COUNTRY' and 'COUNTRYAFF' columns.
-    """
-    column_list = ['Location', 'CountryName_FromSource']
-
-    country_list = set()
-
-    # Collect unique country values from specified columns
-    for column in column_list:
-        country_list |= set(df[df['COUNTRY'].isnull()][column].unique())
-
-    # Iterate over country values
-    for country in country_list:
-        # Iterate over specified columns
-        for column in column_list:
-            # Get subset of rows where 'COUNTRY' is null and 'column' matches the country value
-            subset = df[(df['COUNTRY'].isnull()) & (df[column] == country)]
-
-            # Iterate over subset rows
-            for index, row in subset.iterrows():
-                # Check for matching country name, alpha-3 code, or alpha-2 code
-                for country_name in pycountry.countries:
-                    country_lower = country.strip().lower()
-
-                    if country_name.name.lower() in country_lower:
-                        df.at[index, 'COUNTRY'] = country_name.name
-                        df.at[index, 'COUNTRYAFF'] = country_name.name
-                        break
-                    if country_name.alpha_3 in country_lower:
-                        df.at[index, 'COUNTRY'] = country_name.name
-                        df.at[index, 'COUNTRYAFF'] = country_name.name
-                        break
-                    if country_name.alpha_2 in country_lower:
-                        df.at[index, 'COUNTRY'] = country_name.name
-                        df.at[index, 'COUNTRYAFF'] = country_name.name
-                        break
-
-    return df
-
-
-def fill_country_code(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Fills missing values in the 'ISO_CC' column based on the 'COUNTRY' column using pycountry.
-
-    Parameters:
-        df (pd.DataFrame): The input DataFrame.
-
-    Returns:
-        pd.DataFrame: The DataFrame with updated 'ISO_CC' column.
-    """
-    # Create a mapping of countries to country codes
-    country_mapping = {
-        country.name: country.alpha_2
-        for country in pycountry.countries
-    }
-
-    # Iterate over rows with missing values in 'ISO_CC' column
-    for index, row in df[df['ISO_CC'].isna() & ~df['COUNTRY'].isna()].iterrows():
-        country = row['COUNTRY']
-
-        # Check if country exists in the mapping
-        if country in country_mapping:
-            # Update the 'ISO_CC' column with the country code
-            df.at[index, 'ISO_CC'] = country_mapping[country]
-
+            progress = (f"{i}/{country_len} {row['COUNTRY']}")
+            echo(progress)
     return df
 
 
 def fill_country_subdivision(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Fills missing values in the 'ISO_SUB', 'NAME', and 'LAND_TYPE' columns based on the 'Location' column using pycountry.
-
+    Fills missing values in the 'NAME', and 'LAND_TYPE' columns based on the 'Location' column using pycountry.
     Parameters:
         df (pd.DataFrame): The input DataFrame.
-
     Returns:
-        pd.DataFrame: The DataFrame with updated 'ISO_SUB', 'NAME', and 'LAND_TYPE' columns.
+        pd.DataFrame: The DataFrame with updated 'NAME', and 'LAND_TYPE' columns.
     """
     # Create a mapping of subdivision names to subdivision codes
     subdivision_mapping = {
-        subdivision.name.lower(): subdivision.code
+        subdivision.name: subdivision.code
         for subdivision in pycountry.subdivisions
     }
 
-    # Iterate over rows with missing values in 'ISO_SUB' column
-    for index, row in df[df['ISO_SUB'].isna()].iterrows():
+    # Iterate over rows with missing values in 'NAME' column
+    for index, row in df[df['NAME'].isnull()].iterrows():
         location = str(row['Location'])
 
         matching_subdivision = None
@@ -251,13 +193,12 @@ def fill_country_subdivision(df: pd.DataFrame) -> pd.DataFrame:
 
         # Look for exact matches of subdivision names in the 'Location' column
         for subdivision_name, subdivision_code in subdivision_mapping.items():
-            if subdivision_name in location:
+            if unidecode(subdivision_name.lower()) in unidecode(location.lower()):
                 matching_subdivision = subdivision_name
                 break
 
         if matching_subdivision:
             # Update the corresponding columns with the subdivision information
-            df.at[index, 'ISO_SUB'] = subdivision_code
             df.at[index, 'NAME'] = pycountry.subdivisions.get(
                 code=subdivision_code).name
             df.at[index, 'LAND_TYPE'] = pycountry.subdivisions.get(
@@ -276,11 +217,9 @@ def fill_country_subdivision(df: pd.DataFrame) -> pd.DataFrame:
                     matching_subdivision = subdivision_name
 
                     # Update the corresponding columns with the subdivision information
-                    df.at[index, 'ISO_SUB'] = subdivision_code
                     df.at[index, 'NAME'] = pycountry.subdivisions.get(
                         code=subdivision_code).name
                     df.at[index, 'LAND_TYPE'] = pycountry.subdivisions.get(
                         code=subdivision_code).type
                     break
-
     return df
