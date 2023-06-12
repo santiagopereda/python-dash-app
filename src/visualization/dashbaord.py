810e1e1c69e4df8e1cc80a0f388b7f703321d09f
Santiago Pereda <santiago.pereda1@gmail.com>
2023-06-12 09:29:15 +0200

    Update: Dash Bootstrap Components

--- src/visualization/dashbaord.py
+++ src/visualization/dashbaord.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import dash
 from dash import Dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 
 
 # ======================================================================================================================
 # Import Pandas Dataframe & Initialize APP
 # ======================================================================================================================
-app = Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 token = "pk.eyJ1Ijoic2FudGlhZ29wZXJlZGEiLCJhIjoiY2xpbm1wZG9qMDAyZTNtbGJsZDJjYWN2NyJ9.i66XD8Qd9IF3tjhRAV5oNg"
 pickle_loc = "../../data/interim/01_data_processed.pkl"
@@ -16,19 +18,16 @@ df = pd.read_pickle(pickle_loc)
 # ======================================================================================================================
 # App layout
 # ======================================================================================================================
-app.layout = html.Div([
-    html.Div([
+app.layout = dbc.Container([
+    dbc.Row([
         html.Div([
-            html.Div([
-                html.H1("Plastic Waste Collected for Earth Day Challenge",
-                        style={'text-align': 'center', 'color': 'white'}),
-            ]),
-        ], className="six column", id="title")
-    ], id="header", className="row flex-display", style={"margin-bottom": "25px"}),
+            html.H5("Global Earth Challenge Report",
+                    style={'text-left': 'center', 'color': 'white'})
 
-    html.Div([
-        html.Div([
-            dcc.RangeSlider(
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col([dcc.RangeSlider(
                 id='slct_year',
                 min=df.index.year.min(),
                 max=df.index.year.max(),
@@ -38,18 +37,24 @@ app.layout = html.Div([
                 # marks = {i: f'{i}' for i in range(df.index.year.min(), (df.index.year.max()+1))},
                 allowCross=False,
                 tooltip={"placement": "bottom", "always_visible": True},
-                className='dark-mode'
-            ),
-        ], className="create_container 12 columns"),
-    ], className="row flex-display"),
+                className='dark-mode'),
 
-    html.Div(id='output_container', children=[]),
-    html.Br(),
+            html.Div(id='output_container', children=[]),
 
-    dcc.Graph(id='eac_map', figure={})
-],
-    className='dark-mode-container'
-)
+            html.Br(),
+        ]),
+        dbc.Col([
+            dcc.Graph(id='eac_map', figure={})
+        ]),
+        dbc.Col([
+
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col(html.H1("Plastic Waste Collected for Earth Day Challenge",
+                        style={'text-align': 'center', 'color': 'white'})),
+    ])
+], fluid=True,)
 
 # ======================================================================================================================
 # Connect the Plotly graphs with Dash Components
@@ -72,12 +77,28 @@ def update_graph(option_slctd):
     container = ["There are {} values in this range".format(
         len(filtered_data))]
 
-    fig = px.scatter_mapbox(filtered_data, lat='Latitude1', lon='Longitude1', hover_name="Location",
-                            hover_data=['DateStandardized', 'Organization'],
-                            color_discrete_sequence=["lightgreen"], zoom=1, height=300, opacity=0.85)
+    fig = px.scatter_mapbox(
+        filtered_data,
+        lat='Latitude1',
+        lon='Longitude1',
+        hover_name="Location",
+        hover_data=['DateStandardized', 'Organization'],
+        color_discrete_sequence=["lightgreen"],
+        zoom=1,  # Set the initial zoom level
+        center=dict(lat=20, lon=15),  # Set the initial center location
+        width=800,
+        height=400,
+        opacity=0.85
+    )
     fig.update_layout(
-        mapbox_style="mapbox://styles/santiagopereda/clinlvecs002d01p73lb45noj", mapbox_accesstoken=token)
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        mapbox_style="mapbox://styles/santiagopereda/clinlvecs002d01p73lb45noj",
+        mapbox_accesstoken=token,
+        uirevision="Don't change",
+        paper_bgcolor="#242424",
+        autosize=True,
+        margin=go.layout.Margin(l=0, r=0, t=0,  b=0),
+        showlegend=False
+    )
 
     return container, fig
 
@@ -86,4 +107,4 @@ def update_graph(option_slctd):
 # Run Dashboard
 # ======================================================================================================================
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8049, use_reloader=False)
+    app.run_server(debug=True, port=8049)
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -4,24 +4,47 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import dash
+from dash import Dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 
-pickle_loc = "../../data/interim/01_data_processed.pkl"
-df = pd.read_pickle(pickle_loc)
-df.columns
+pickle_loc_1 = "../../data/interim/01_data_processed.pkl"
+pickle_loc_2 = "../../data/interim/02_data_processed.pkl"
+
+df_1 = pd.read_pickle(pickle_loc_1)
+df_2 = pd.read_pickle(pickle_loc_2)
 token = "pk.eyJ1Ijoic2FudGlhZ29wZXJlZGEiLCJhIjoiY2xpbm1wZG9qMDAyZTNtbGJsZDJjYWN2NyJ9.i66XD8Qd9IF3tjhRAV5oNg"
 # --------------------------------------------------------------
-# Plot single columns
+# Mapbox
 # --------------------------------------------------------------
-fig = px.scatter_mapbox(df, lat='Latitude1', lon='Longitude1', hover_name="Location",
-                        hover_data=['DateStandardized', 'Organization'],
-                        color_discrete_sequence=["lightcoral"], zoom=1, height=300, opacity = 0.85)
-fig.update_layout(mapbox_style="mapbox://styles/santiagopereda/clinlvecs002d01p73lb45noj", mapbox_accesstoken=token)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
-# --------------------------------------------------------------
-# Plot all exercises
-# --------------------------------------------------------------
+fig_1 = px.scatter_mapbox(
+        df_1,
+        lat='Latitude1',
+        lon='Longitude1',
+        hover_name="Location",
+        hover_data=['DateStandardized', 'Organization'],
+        color_discrete_sequence=["lightgreen"],
+        zoom=1,  # Set the initial zoom level
+        center=dict(lat=15, lon=15),  # Set the initial center location
+        width=800,
+        height=400,
+        opacity=0.85
+    )
 
+fig_1.update_layout(
+        mapbox_style="mapbox://styles/santiagopereda/clinlvecs002d01p73lb45noj",
+        mapbox_accesstoken=token,
+        uirevision="Don't change",
+        paper_bgcolor="#242424",
+        autosize=True,
+        margin=go.layout.Margin(l=0, r=0, t=0,  b=0),
+        showlegend=False
+    )
+fig_1.show()
+# --------------------------------------------------------------
+# Pie Charts
+# --------------------------------------------------------------
+df_2.groupby(level=0)
 
 # --------------------------------------------------------------
 # Adjust plot settings
